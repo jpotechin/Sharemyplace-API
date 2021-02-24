912eb6c68f3a49a43b5db8a5ada854190b45ccfe
jpotechin <josephpotechin@gmail.com>
2021-02-23 19:03:43 -0800

    Photo Uploader

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -35,7 +35,7 @@ namespace API.Controllers
 			return Ok(users);
 		}
 		// api/users/username
-		[HttpGet("{username}")]
+		[HttpGet("{username}", Name = "GetUser")]
 		public async Task<ActionResult<MemberDto>> GetUser(string username)
 		{
 			return await _userRepository.GetMemberAsync(username);
@@ -78,7 +78,12 @@ namespace API.Controllers
 			user.Photos.Add(photo);
 
 			if (await _userRepository.SaveAllAsync())
-				return _mapper.Map<PhotoDto>(photo);
+			{
+				// return CreatedAtRoute("GetUser", _mapper.Map<PhotoDto>(photo));
+				return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+			}
+
+				
 			
 			return BadRequest("Problem adding photo");
 
