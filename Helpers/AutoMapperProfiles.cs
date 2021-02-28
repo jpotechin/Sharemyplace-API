125bb1c45f16ea62fb8358382371c532301d90f0
jpotechin <josephpotechin@gmail.com>
2021-02-28 08:49:46 -0800

    Setup Auto mapper and create message dto

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -17,6 +17,19 @@ namespace API.Helpers
                 src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
 			CreateMap<MemberUpdateDto, AppUser>();
+			CreateMap<Message, MessageDto>()
+				.ForMember(
+					dest => dest.SenderPhotoUrl, 
+					opt => opt.MapFrom(
+						src => src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url
+						)
+				)
+				.ForMember(
+					dest => dest.RecipientPhotoUrl, 
+					opt => opt.MapFrom(
+						src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url
+						)
+				);
 		}
 	}
 }
