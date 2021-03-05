8be08ac8d2e60aa995341631d92f411af730c84e
jpotechin <josephpotechin@gmail.com>
2021-03-05 07:06:16 -0800

    Start point for implementing Identity

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,81 +6,78 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController : BaseApiController
-    {
-        private readonly DataContext _context;
-        private readonly ITokenService _tokenService;
-        public AccountController(DataContext context, ITokenService tokenService)
-        {
-            _tokenService = tokenService;
-            _context = context;
+	public class AccountController : BaseApiController
+	{
+		private readonly DataContext _context;
+		private readonly ITokenService _tokenService;
+		private readonly Mapper _mapper;
+		public AccountController(DataContext context, ITokenService tokenService, Mapper mapper)
+		{
+			_mapper = mapper;
+			_tokenService = tokenService;
+			_context = context;
 
-        }
+		}
 
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
+		[HttpPost("register")]
+		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+		{
 
-            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+			if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+			// var user = new AppUser
+			// {
+			//     UserName = registerDto.Username.ToLower(),
+			// };
 
-            using var hmac = new HMACSHA512();
+			// _context.Users.Add(user);
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+			// await _context.SaveChangesAsync();
+
+			var user = _mapper.Map<AppUser>(registerDto);
+
+            user.UserName = registerDto.Username.ToLower();
 
             _context.Users.Add(user);
-
             await _context.SaveChangesAsync();
 
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs,
-                Gender = user.Gender
-            };
-        }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-        {
-            var user = await _context.Users
-            .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+						return new UserDto
+						{
+							Username = user.UserName,
+							Token = _tokenService.CreateToken(user),
+							KnownAs = user.KnownAs,
+							Gender = user.Gender
+						};
+		}
 
-            if (user == null) return Unauthorized("Invalid Username");
+		[HttpPost("login")]
+		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+		{
+			var user = await _context.Users
+			.Include(p => p.Photos)
+			.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-            using var hmac = new HMACSHA512(user.PasswordSalt);
+			if (user == null) return Unauthorized("Invalid Username");
 
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+			return new UserDto
+			{
+				Username = user.UserName,
+				Token = _tokenService.CreateToken(user),
+				PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url,
+				KnownAs = user.KnownAs,
+				Gender = user.Gender
+			};
+		}
 
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
-
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url,
-                KnownAs = user.KnownAs,
-                Gender = user.Gender
-            };
-        }
-
-        private async Task<bool> UserExists(string username)
-        {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
-        }
-    }
+		private async Task<bool> UserExists(string username)
+		{
+			return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+		}
+	}
 }
--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -1,17 +1,11 @@
 using System;
 using System.Collections.Generic;
-using Api.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
         public DateTime DateOfBirth {get; set;}
         public string KnownAs { get; set; }
@@ -31,5 +25,6 @@ namespace API.Entities
 
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
