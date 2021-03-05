3fdcb1a0258b06fa75670e2fff8d9d56e9a9d067
jpotechin <josephpotechin@gmail.com>
2021-03-05 15:05:47 -0800

    Updated Seed class and reseed db with identity setup

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -1,20 +1,19 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
 	public class Seed
 	{
-		public static async Task SeedUsers(DataContext context)
+		public static async Task SeedUsers(UserManager<AppUser> userManager)
 		{
 	
-			if (await context.Users.AnyAsync()) return;
+			if (await userManager.Users.AnyAsync()) return;
 
 			var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
 
@@ -25,10 +24,8 @@ namespace API.Data
 				using var hmac = new HMACSHA512();
 
 				user.UserName = user.UserName.ToLower();
-
-				context.Users.Add(user);
+				await userManager.CreateAsync(user, "Pa$$w0rd");
 			}
-            await context.SaveChangesAsync();
 		}
 	}
 }
